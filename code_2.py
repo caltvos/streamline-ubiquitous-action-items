918aded4424abed750c005b0aa6e04bf8bb0ae47
caltvos <152297554+caltvos@users.noreply.github.com>
2023-12-17 06:01:24 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,27 @@
+    main()
+def main():
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    for item in data:
+import random
+
+if __name__ == "__main__":
+def main():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+    return data
+if __name__ == "__main__":
+    data = generate_random_data()
+
+
+    for item in data:
+import random
+
+        print(f"Random Number: {item}")
